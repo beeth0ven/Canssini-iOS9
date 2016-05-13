bd45c49451e059068f40a7874419eced22ce2890
beeth0ven <beeth0vendev@163.com>
2016-05-13 23:30:47 +0800

    Initial Commit

---
+++ Canssini-iOS9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Canssini-iOS9
+//
+//  Created by luojie on 16/5/13.
+//  Copyright © 2016年 LuoJie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
